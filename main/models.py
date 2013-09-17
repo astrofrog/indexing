887b65744766dadbf39e2c492617d356e85093f0
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-09-17 13:28:21 -0400

    Updated models definition

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-# See https://docs.djangoproject.com/en/dev/ref/models/fields/#datetimefield for a list of field classes.
+
+# See:
+# https://docs.djangoproject.com/en/dev/ref/models/fields/#datetimefield
+# for a list of field classes.
 
 
 class QuantityDefinition(models.Model):
@@ -11,19 +14,36 @@ class QuantityDefinition(models.Model):
     description = models.TextField()
 
 
+class User(models.Model):
+    """
+    Define a user
+    """
+    first = models.CharField(max_length=100)
+    last = models.CharField(max_length=100)
+    api_key = models.CharField(max_length=100)
+    date_joined = models.DateTimeField()
+
+
+class Object(models.Model):
+    """
+    Define an Astronomical object
+    """
+    name = models.CharField(max_length=100)
+    coordinates = models.CharField(max_length=100, blank=True)
+
+
 class Quantity(models.Model):
     """
     A 'well defined quantity'
     """
     quantity = models.ForeignKey(QuantityDefinition)
-    object_name = models.CharField(max_length=100)
+    object = models.ForeignKey(Object)
     value = models.FloatField()
-    uncertainty = models.FloatField()
-    unit = models.CharField(max_length=100)
-    origin = models.URLField()
-    date = models.DateTimeField()
+    uncertainty = models.FloatField(blank=True)
+    unit = models.CharField(max_length=100, blank=True)
+    origin = models.URLField(blank=True)
+    date = models.DateTimeField(blank=True)
     date_entered = models.DateTimeField()
-    user_entered = models.CharField(max_length=100)
+    user = models.ForeignKey(User)
 
 
-# models.ForeignKey(QuantityDefinition)
