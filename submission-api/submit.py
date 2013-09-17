6df3e7f399d08e10a3c151262cfa4678ead98022
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-09-17 14:40:10 -0400

    Added example submit script

---
+++ submission-api/submit.py
@@ -0,0 +1,36 @@
+import urllib
+import urllib2
+
+BASE_URL = 'http://127.0.0.1:8000/main/add_quantity/'
+
+def add_quantity(name, object, value, unit, user, uncertainty=None, origin=None):
+
+    # Required arguments
+    
+    values = {
+              'name':name,
+              'object':object,
+              'value':value,
+              'unit':unit,
+              'user':user
+              }
+              
+    if uncertainty is not None:
+        values['uncertainty'] = uncertainty
+        values['origin'] = origin
+    
+    # Encode the POST request    
+    data = urllib.urlencode(values)
+    
+    # Prepare request
+    req = urllib2.Request(BASE_URL, data)
+
+    # Send request
+    print(req)
+    response = urllib2.urlopen(req)
+    
+    print(response.read())
+
+add_quantity('flux', '51pegb', 1.2, 'mJy', 'tom')
+
+# http://127.0.0.1:8000/main/add_quantity/?a=1
